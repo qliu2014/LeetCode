114009b8e273f03ef5c8ccc68e6139de38b66287
qliu2014 <quan.liu.engr@icloud.com>
2022-04-26 12:06:26 -0400

    Tested compiling and running LeetCode_100.java

--- 100. Same Tree/LeetCode_100.java
+++ 100. Same Tree/LeetCode_100.java
@@ -1,32 +1,23 @@
-import java.util.TreeNode;
+import java.util.*;
 
-class LeetCode_100 {
+public class LeetCode_100 {
 
 	public static void main(String[] args) {
 
-		//call this function
+		//call isSameTree()
 
+		System.out.println(res.toString());
 
+	}
 
-
-	} 
-
-
-
-
-
-
-
-
-
-  	public boolean isSameTree(TreeNode p, TreeNode q) {
-	    // p and q are both null
-	    if (p == null && q == null) return true;
-	    // one of p and q is null
-	    if (q == null || p == null) return false;
-	    if (p.val != q.val) return false;
-	    return isSameTree(p.right, q.right) &&
-	            isSameTree(p.left, q.left);
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+    // p and q are both null
+    if (p == null && q == null) return true;
+    // one of p and q is null
+    if (q == null || p == null) return false;
+    if (p.val != q.val) return false;
+    return isSameTree(p.right, q.right) &&
+            isSameTree(p.left, q.left);
   }
 
 
